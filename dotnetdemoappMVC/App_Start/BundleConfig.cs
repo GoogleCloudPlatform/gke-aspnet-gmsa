9bd250c0677ed97ddd1ddec8fe941beb568e6c4c
Ido Flatow <idoflatow@google.com>
2023-07-09 21:07:03 +0300

    Update BundleConfig.cs

--- dotnetdemoappMVC/App_Start/BundleConfig.cs
+++ dotnetdemoappMVC/App_Start/BundleConfig.cs
@@ -22,7 +22,7 @@ namespace DotNetDemoAppMvc
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {                     
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery/jquery.js"));            
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery/dist/jquery.js"));            
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
